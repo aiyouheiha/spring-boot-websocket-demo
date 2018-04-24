1a2d235aaa8bbb5d57eea9b0e89ddf885cba73a9
singoasher <singoasher@163.com>
2018-04-24 10:34:28 +0800

    提高代码可读性

--- src/main/java/com/heiha/demo/springboot/websocket/SpringBootWebsocketDemoApplication.java
+++ src/main/java/com/heiha/demo/springboot/websocket/SpringBootWebsocketDemoApplication.java
@@ -3,6 +3,10 @@ package com.heiha.demo.springboot.websocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author singoasher
+ * @date 2018/4/24
+ */
 @SpringBootApplication
 public class SpringBootWebsocketDemoApplication {
 
--- src/main/java/com/heiha/demo/springboot/websocket/web/DemoWebSocket.java
+++ src/main/java/com/heiha/demo/springboot/websocket/web/DemoWebSocket.java
@@ -1,5 +1,7 @@
 package com.heiha.demo.springboot.websocket.web;
 
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
@@ -8,35 +10,47 @@ import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
- * <br>
- * <b>Project:</b> spring-boot-websocket-demo<br>
- * <b>Date:</b> 2017/9/8 15:35<br>
- * <b>Author:</b> heiha<br>
+ * @author singoasher
+ * @date 2017/9/8
  */
+@Slf4j
+@EqualsAndHashCode
 @ServerEndpoint(value = "/websocket")
 @Component
 public class DemoWebSocket {
-    //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
+    private final static String STATUS_CONNECTED = "connected";
+
+    /**
+     * 静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
+     */
     private static int onlineCount = 0;
 
-    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
+    /**
+     * concurrent 包的线程安全 Set，用来存放每个客户端对应的 DemoWebSocket 对象。
+     */
     private static CopyOnWriteArraySet<DemoWebSocket> webSocketSet = new CopyOnWriteArraySet<>();
 
-    //与某个客户端的连接会话，需要通过它来给客户端发送数据
+    /**
+     * 与某个客户端的连接会话，需要通过它来给客户端发送数据
+     */
     private Session session;
 
     /**
-     * 连接建立成功调用的方法*/
+     * 连接建立成功调用的方法
+     */
     @OnOpen
     public void onOpen(Session session) {
         this.session = session;
-        webSocketSet.add(this);     //加入set中
-        addOnlineCount();           //在线数加1
-        System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
+        // 加入set中
+        webSocketSet.add(this);
+        // 在线数加1
+        addOnlineCount();
+
+        log.info("有新连接加入！当前在线人数 {}", getOnlineCount());
         try {
-            sendMessage("");
+            sendMessage(STATUS_CONNECTED);
         } catch (IOException e) {
-            System.out.println("IO异常");
+            log.error(e.getMessage());
         }
     }
 
@@ -45,27 +59,27 @@ public class DemoWebSocket {
      */
     @OnClose
     public void onClose() {
-        webSocketSet.remove(this);  //从set中删除
-        subOnlineCount();           //在线数减1
-        System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
+        // 从 set 中删除
+        webSocketSet.remove(this);
+        // 在线数减 1
+        subOnlineCount();
+        log.info("有一连接关闭！当前在线人数 {}", getOnlineCount());
     }
 
     /**
      * 收到客户端消息后调用的方法
      *
-     * @param message 客户端发送过来的消息*/
+     * @param message 客户端发送过来的消息
+     * @param session Session
+     */
     @OnMessage
     public void onMessage(String message, Session session) {
-        System.out.println("来自客户端的消息:" + message);
+        String sessionId = session.getId();
+        log.info("From client: {}, message: {}", sessionId, message);
 
-        //群发消息
-        for (DemoWebSocket item : webSocketSet) {
-            try {
-                item.sendMessage(message);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        // 群发消息
+        String massMessage = message + " | From " + sessionId;
+        sendMassMessage(massMessage);
     }
 
     /**
@@ -73,38 +87,39 @@ public class DemoWebSocket {
      */
      @OnError
      public void onError(Session session, Throwable error) {
-     System.out.println("发生错误");
-     error.printStackTrace();
+         log.error("Error happened, client: {}, error: {}", session.getId(), error.getMessage());
      }
 
 
-     public void sendMessage(String message) throws IOException {
+     private void sendMessage(String message) throws IOException {
          this.session.getBasicRemote().sendText(message);
      }
 
 
      /**
       * 群发自定义消息
-      * */
-    public static void sendInfo(String message) throws IOException {
+      *
+      * @param massMessage Message to send to all
+      */
+    private static void sendMassMessage(String massMessage) {
         for (DemoWebSocket item : webSocketSet) {
             try {
-                item.sendMessage(message);
+                item.sendMessage(massMessage);
             } catch (IOException e) {
-                continue;
+                log.error(e.getMessage());
             }
         }
     }
 
-    public static synchronized int getOnlineCount() {
+    private static synchronized int getOnlineCount() {
         return onlineCount;
     }
 
-    public static synchronized void addOnlineCount() {
+    private static synchronized void addOnlineCount() {
         DemoWebSocket.onlineCount++;
     }
 
-    public static synchronized void subOnlineCount() {
+    private static synchronized void subOnlineCount() {
         DemoWebSocket.onlineCount--;
     }
 }
