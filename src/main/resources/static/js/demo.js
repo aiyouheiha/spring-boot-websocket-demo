668cb0ad21831b85008621dfa9f128eef1b08b3d
singoasher <singoasher@163.com>
2018-04-24 10:07:16 +0800

    提高代码可读性；同时存在原生 js 以及 jquery 的使用方法，作为使用参考

--- src/main/resources/static/js/demo.js
+++ src/main/resources/static/js/demo.js
@@ -1,64 +1,101 @@
-var websocket = null;
+let websocket = null;
 
-// 判断当前浏览器是否支持WebSocket
-if ('WebSocket' in window) {
-    websocket = new WebSocket("ws://localhost:8080/websocket");
-} else {
-    alert('Not support websocket')
-}
-
-// 连接发生错误的回调方法
-websocket.onerror = function(){
-    setMessageInnerHTML("error");
-};
-
-// 连接成功建立的回调方法
-websocket.onopen = function(event) {
-    console.log("connected");
-};
-
-// 接收到消息的回调方法
-websocket.onmessage = function(event) {
-    setMessageInnerHTML(event.data);
-};
-
-// 连接关闭的回调方法
-websocket.onclose = function(){
-    alert("Closed");
-};
+connectWebSocket();
 
 // 监听窗口关闭事件，当窗口关闭时，主动关闭websocket连接
-window.onbeforeunload = function(){
+window.onbeforeunload = function () {
     websocket.close();
 };
 
-// 将消息显示在网页上
-function setMessageInnerHTML(innerHTML){
-    document.getElementById('message').innerHTML
-        += '<hr>' + currentTime() + ' >>> '  + innerHTML;
+function connectWebSocket() {
+    if (websocket !== null) {
+        let readyState = websocket.readyState;
+        console.log("websocket.readyState: " + readyState);
+        if (readyState === WebSocket.CONNECTING || readyState === WebSocket.OPEN) {
+            alert("Already Connected");
+            return
+        }
+    }
+
+    // 判断当前浏览器是否支持 WebSocket
+    if ('WebSocket' in window) {
+        websocket = new WebSocket("ws://localhost:8080/websocket");
+        bindWebSocketEvent();
+    } else {
+        alert('WebSocket Not Support');
+    }
 }
 
-//关闭连接
-function closeWebSocket(){
+function bindWebSocketEvent() {
+    // 连接发生错误的回调方法
+    websocket.onerror = function () {
+        setMessageInnerHTML("error");
+    };
+
+    // 连接成功建立的回调方法
+    websocket.onopen = function (event) {
+        let status = 'connected';
+        console.log(status);
+        // 等价于 document.getElementById('status-label').innerHTML = status;
+        $('#status-label').html(status);
+    };
+
+    // 接收到消息的回调方法
+    websocket.onmessage = function (event) {
+        let message = event.data;
+        console.log("on message: " + message);
+        setMessageInnerHTML(message);
+    };
+
+    // 连接关闭的回调方法
+    websocket.onclose = function () {
+        let status = 'closed';
+        console.log(status);
+        // 等价于 document.getElementById('status-label').innerHTML = status;
+        $('#status-label').html(status);
+    };
+}
+
+/**
+ * 将消息显示在网页上
+ *
+ * @param innerHTML
+ */
+function setMessageInnerHTML(innerHTML) {
+    document.getElementById('message').innerHTML
+        += '<hr>' + currentTime() + ': '  + innerHTML;
+}
+
+/**
+ * 关闭连接
+ */
+function closeWebSocket() {
     websocket.close();
 }
 
-//发送消息
-function send(){
-    var message = document.getElementById('text').value;
+/**
+ * 发送消息
+ */
+function send() {
+    let message = document.getElementById('text').value;
     websocket.send(message);
 }
 
+/**
+ * Current Time
+ *
+ * @returns {string}
+ */
 function currentTime() {
-    var now = new Date();
-    var year = now.getFullYear();
-    var month = now.getMonth() + 1;
-    var day = now.getDate();
-    var hh = now.getHours();
-    var mm = now.getMinutes();
-    var ss = now.getSeconds();
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let day = now.getDate();
+    let hh = now.getHours();
+    let mm = now.getMinutes();
+    let ss = now.getSeconds();
 
-    var clock = year + "-";
+    let clock = year + "-";
     if(month < 10) clock += "0";
     clock += month + "-";
     if(day < 10) clock += "0";
